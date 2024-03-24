f7dcb99e4071c049a1bb5c2edd54f8b1dabf6c10
SuKiN-a <suryakiranpradosh@gmail.com>
2024-03-24 10:51:28 +0200

    added docs and README

---
+++ README.md
@@ -0,0 +1,19 @@
+# Charty
+
+Maven is required to compile charty.
+
+This project has only been tested on EndeavourOS Linux.
+
+
+## Running the app
+
+```sh
+mvn javafx:run
+```
+
+## Generating API documentation
+The generated docs will be present in docs/charty/
+
+```sh
+mvn javadoc:javadoc
+```
---
+++ docs/charty/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"charty","l":"App"},{"p":"charty","l":"Chart"},{"p":"charty","l":"ImageExporter"},{"p":"charty","l":"Line"},{"p":"charty","l":"Pie"},{"p":"charty","l":"SerializableChartProxy"},{"p":"charty","l":"UI"}];updateSearchResults();
